f91cf68459cbc61a27e1c6900ae0a8a3405e0ab0
Shivan Modha <shivanmodha@gmail.com>
2018-02-01 08:57:29 -0500

    Code Cleanup

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-let mouseDown;
+// 
 let canvasElement;
 let canvasContext;
-let rx = 700;
-let ry = 300;
+// representing the rectangle position (x, y)
+let point = {};
 function main()
 {
-    canvasElement = document.getElementById("CanvasPainter");
+    canvasElement = document.getElementById("canvas");
     let canvasElementStyle = window.getComputedStyle(canvasElement);
     canvasElement.width = parseInt(canvasElementStyle.width);
     canvasElement.height = parseInt(canvasElementStyle.height);
+    // center the rectangle
+    point.x = canvasElement.width / 2;
+    point.y = canvasElement.height / 2;
+    point.x -= 25;
+    point.y -= 25;
     canvasContext = canvasElement.getContext("2d");
     if (!canvasContext)
     {
         alert("Error: Couldn't create canvasContext");
     }
-    canvasContext.fillStyle = "red";
-    canvasContext.beginPath();
-    canvasContext.rect(rx,ry,50,50);
-    canvasContext.closePath();
-    canvasContext.fill();
-    canvasContext.stroke();
-  //  console.log("asdf");
-//    canvasElement.addEventListener('keydown',check,true);
-    //console.log("jkl;");
+    window.onkeydown = () =>
+    {
+        let code = event.keyCode;
+        switch (code)
+        {
+            case 37:
+                console.log("Left");
+                point.x += -5;
+                break; //Left key
+
+            case 38:
+                console.log("Up");
+                point.y += -5;
+                break; //Up key
+
+            case 39:
+                console.log("Right");
+                point.x += 5;
+                break; //Right key
+
+            case 40:
+                console.log("Down");
+                point.y += 5;
+                break; //Down key
+
+            default: console.log(code); //Everything else
+        }
+        Draw();
+    }
+    Draw();
 }
 function Draw()
 {
@@ -30,43 +56,11 @@ function Draw()
     canvasContext.fillStyle = "white";
     canvasContext.strokeStyle = "black"
     canvasContext.rect(0, 0, canvasElement.width, canvasElement.height);
-    
     canvasContext.fillStyle = "red";
     canvasContext.beginPath();
-    canvasContext.rect(rx,ry,50,50);
+    canvasContext.rect(point.x, point.y, 50, 50);
     canvasContext.closePath();
     canvasContext.fill();
     canvasContext.stroke();
 }
-
-function check(event) {
-    console.log("qwer");
-    var code = event.keyCode;
-    switch (code) {
-        case 37: 
-            console.log("Left");
-            rx+=-5;
-            break; //Left key
-            
-        case 38: 
-            console.log("Up"); 
-            ry+=-5;
-            break; //Up key
-            
-        case 39: 
-            console.log("Right"); 
-            rx+=5;
-            break; //Right key
-            
-        case 40: 
-            console.log("Down"); 
-            ry+=5;
-            break; //Down key
-            
-        default: alert(code); //Everything else
-    }
-    Draw();
-}
-
 main();
-window.addEventListener('keydown',check,true);
